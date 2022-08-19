4fa70a612aab18736cbc37e8b5e48681f5cdd726
ivanAnapa <vanya-cherniy@mail.ru>
2022-08-19 19:37:15 +0300

    last test broken

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -11,7 +11,6 @@ import ru.netology.web.page.TransferPage;
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
-    private final int transferSum = 1000;
 
     @BeforeEach
     void setup() {
@@ -28,11 +27,12 @@ class MoneyTransferTest {
         DashboardPage dashboardPage = new DashboardPage();
         int firstCardBalanceAfter = dashboardPage.getCardBalance(1);
         int secondCardBalanceAfter = dashboardPage.getCardBalance(2);
+        int transferSum = DataHelper.getTransferSum();
         dashboardPage.clickDepositByCardOrder(1);
 
         TransferPage transferPage = new TransferPage();
         transferPage.checkOpenedPage();
-        transferPage.setTransferSum(DataHelper.getTransferSum());
+        transferPage.setTransferSum(transferSum);
         transferPage.setCardFrom(DataHelper.getCardNumbers().getSecondCardNumber());
         transferPage.clickTransferBtn();
 
@@ -40,7 +40,6 @@ class MoneyTransferTest {
         int secondCardBalanceBefore = dashboardPage.getCardBalance(2);
         Assertions.assertEquals(firstCardBalanceAfter + transferSum, firstCardBalanceBefore);
         Assertions.assertEquals(secondCardBalanceAfter - transferSum, secondCardBalanceBefore);
-        System.out.println("1 test - OK");
     }
 
     @Test
@@ -48,6 +47,7 @@ class MoneyTransferTest {
         DashboardPage dashboardPage = new DashboardPage();
         int firstCardBalanceAfter = dashboardPage.getCardBalance(1);
         int secondCardBalanceAfter = dashboardPage.getCardBalance(2);
+        int transferSum = DataHelper.getTransferSum();
         dashboardPage.clickDepositByCardOrder(2);
 
         TransferPage transferPage = new TransferPage();
@@ -60,8 +60,6 @@ class MoneyTransferTest {
         int secondCardBalanceBefore = dashboardPage.getCardBalance(2);
         Assertions.assertEquals(firstCardBalanceAfter - transferSum, firstCardBalanceBefore);
         Assertions.assertEquals(secondCardBalanceAfter + transferSum, secondCardBalanceBefore);
-        System.out.println("2 test - OK");
-
     }
 
     @Test
@@ -69,6 +67,7 @@ class MoneyTransferTest {
         DashboardPage dashboardPage = new DashboardPage();
         int firstCardBalanceAfter = dashboardPage.getCardBalance(1);
         int secondCardBalanceAfter = dashboardPage.getCardBalance(2);
+        int transferSum = DataHelper.getTransferSum();
         dashboardPage.clickDepositByCardOrder(1);
 
         TransferPage transferPage = new TransferPage();
@@ -80,9 +79,7 @@ class MoneyTransferTest {
         int firstCardBalanceBefore = dashboardPage.getCardBalance(1);
         int secondCardBalanceBefore = dashboardPage.getCardBalance(2);
         Assertions.assertEquals(firstCardBalanceAfter, firstCardBalanceBefore);
-        Assertions.assertEquals(secondCardBalanceAfter, secondCardBalanceBefore);
-        System.out.println("3 test - OK");
-
+        Assertions.assertEquals(secondCardBalanceAfter, secondCardBalanceBefore + 1);
     }
 
 }
